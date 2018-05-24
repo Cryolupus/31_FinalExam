c5e8e283dc22b052ff82bf477da9acfd2e3a8a7f
Kevin Chou <chouk@rose-hulman.edu>
2018-05-24 13:21:37 -0400

    /

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Kevin Chou.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -90,9 +90,13 @@ def problem1a(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    total = 0
+    total = numbers[0] + numbers[len(numbers)-1] + numbers[(len(numbers)//2)]
+    return total
+
 
 
 def run_test_problem1b():
@@ -166,10 +170,14 @@ def problem1b(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    a = len(numbers)/3
+    total = 0
+    for k in range(a):
+        total = total + numbers[a+k]
+    return total
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
